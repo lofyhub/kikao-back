c06a9debe59e146de0b467c9f6c7c17262d87946
John Philip <developerphilo@gmail.com>
2023-01-30 13:21:07 +0300

    feat: init bookmarks

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import mongoose from 'mongoose';
 import sanitize from 'mongo-sanitize';
 
@@ -72,8 +72,65 @@ async function getUserListings(req: Request, res: Response) {
     }
 }
 
+async function addFavourite(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { Id, userid } = req.body;
+        const userId = userid;
+        if (!Id || !userId) {
+            return res
+                .status(309)
+                .json({ message: 'Id and userId are required' });
+        }
+
+        const collection = await mongoose.connection.db.collection('bookmarks');
+        const bookmarkUser = await collection.findOne({ userId });
+
+        let newBookmarks;
+        if (!bookmarkUser) {
+            newBookmarks = [Id];
+            await collection.insertOne({ userId, bookmarks: newBookmarks });
+        } else {
+            newBookmarks = [...bookmarkUser.bookmarks, Id];
+            const { modifiedCount } = await collection.updateOne(
+                { userId },
+                { $set: { bookmarks: newBookmarks } }
+            );
+            if (!modifiedCount) {
+                return res
+                    .status(404)
+                    .json({ message: 'Bookmark update was not successful' });
+            }
+        }
+
+        return res
+            .status(200)
+            .json({ message: 'Bookmark request was successful' });
+    } catch (error) {
+        next(error);
+        return;
+    }
+}
+
+async function fetchBookmarks(req: Request, res: Response, next: NextFunction) {
+    const { userid } = req.query;
+    const userId: string = userid as string;
+    if (!userId) {
+        return res.status(309).json({ message: 'userId is required' });
+    }
+    try {
+        const collection = await mongoose.connection.db.collection('bookmarks');
+        const bookmarks = await collection.find({ userId });
+        return res.status(200).json({ bookmarks });
+    } catch (error) {
+        next(error);
+        return;
+    }
+}
+
 // Routes
 router.post('/listing/author', verifyToken, getListingPublisher);
 router.post('/author/listings', verifyToken, getUserListings);
+router.post('/bookmarks', verifyToken, addFavourite);
+router.get('/user/bookmarks', verifyToken, fetchBookmarks);
 
 export default router;
